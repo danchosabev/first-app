ad2a471d1ad64b5562936f62468fb4df5ea23f4a
Dancho Yordanov <danchosabev@gmail.com>
2016-09-07 11:16:58 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  template: `
+  <h1>Title</h1>
+  <h2>Subtitle</h2>
+  `,
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
